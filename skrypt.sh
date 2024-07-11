ded3b0d33c1c660f9f1c7611f7b5f056633ebf96
Honkadaloonga <->
2024-07-11 16:07:57 +0200

    add logs

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/bash
 
-if [ "$1" == "--date" ]; then
-   date
+stworz_pliki_log() {
+   nazwa_skryptu=$(basename "$0")
+   data=$(date)
+
+   for i in {1..100}; do
+      nazwa_pliku=$(printf "log%03d.txt" $i)
+      echo "Nazwa pliku: $nazwa_pliku" > $nazwa_pliku
+      echo "Utworzony przez: $nazwa_skryptu" >> $nazwa_pliku
+      echo "Data: $data" >> $nazwa_pliku
+   done
+}
+
+flaga_daty=false
+flaga_logow=false
+
+for argument in "$@"; do
+  case $argument in
+    --date) flaga_daty=true ;;
+    --logs) flaga_logow=true ;;
+    *) ;;
+  esac
+done
+
+if $flaga_daty; then
+  date
+fi
+
+if $flaga_logow; then
+  stworz_pliki_log
 fi
