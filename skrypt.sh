72511f506aca3bcf4e52a1cee807bdb15dfc7146
Honkadaloonga <->
2024-07-11 16:52:00 +0200

    add log amount option

--- skrypt.sh
+++ skrypt.sh
@@ -4,7 +4,7 @@ stworz_pliki_log() {
    nazwa_skryptu=$(basename "$0")
    data=$(date)
 
-   for i in {1..100}; do
+   for i in $(seq 1 $1); do
       nazwa_pliku=$(printf "log%03d.txt" $i)
       echo "Nazwa pliku: $nazwa_pliku" > $nazwa_pliku
       echo "Utworzony przez: $nazwa_skryptu" >> $nazwa_pliku
@@ -14,19 +14,32 @@ stworz_pliki_log() {
 
 flaga_daty=false
 flaga_logow=false
+ilosc_logow=100
 
-for argument in "$@"; do
-  case $argument in
-    --date) flaga_daty=true ;;
-    --logs) flaga_logow=true ;;
-    *) ;;
-  esac
+while [[ $# -gt 0 ]]; do
+   case $1 in
+      --date)
+         flaga_daty=true
+         shift
+      ;;
+      --logs)
+         flaga_logow=true
+         shift
+         if [[ $1 =~ ^[0-9]+$ ]]; then
+            ilosc_logow=$1
+            shift
+         fi
+      ;;
+      *)
+         shift
+      ;;
+   esac
 done
 
 if $flaga_daty; then
-  date
+   date
 fi
 
 if $flaga_logow; then
-  stworz_pliki_log
+   stworz_pliki_log $ilosc_logow
 fi
